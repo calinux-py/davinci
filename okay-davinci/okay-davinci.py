cd478010908f513ad7ba699921c72cbf3f9e5331
CaliNux <132522491+calinux-py@users.noreply.github.com>
2023-06-15 19:06:46 -0700

    Update okay-davinci.py

--- okay-davinci/okay-davinci.py
+++ okay-davinci/okay-davinci.py
@@ -1,3 +1,3 @@
 This script has been purchased from a third-party. I aplogize for any inconvience.
-You can still find the .py file in forked versions, but as per my agreement, I can not list it publically. 
+You can still find the .py file in forked versions, but as per my agreement, I can not list it publically - or continue to work on it. 
 Please message me for any questions.
